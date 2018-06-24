0c8e1f1eee60756f7d90a277ec2333c2acd9f019
Tyler Davis <datadazer@gmail.com>
2018-06-23 21:52:35 -0600

    start adding cards and robots

---
+++ src/robots.js
@@ -0,0 +1,602 @@
+export const robots = [
+  {
+    "id": 1,
+    "name": "Kyle Dawson",
+    "username": "luctus, ipsum",
+    "email": "elementum@molestiepharetranibh.edu"
+  },
+  {
+    "id": 2,
+    "name": "Rudyard Mcneil",
+    "username": "molestie pharetra",
+    "email": "erat@conubianostraper.org"
+  },
+  {
+    "id": 3,
+    "name": "Benedict Taylor",
+    "username": "sed dolor.",
+    "email": "a.magna@vestibulumneque.net"
+  },
+  {
+    "id": 4,
+    "name": "Perry Gallagher",
+    "username": "Ut semper",
+    "email": "ullamcorper@aliquetliberoInteger.org"
+  },
+  {
+    "id": 5,
+    "name": "Keelie Kane",
+    "username": "In faucibus.",
+    "email": "Nam.nulla@sapiengravida.com"
+  },
+  {
+    "id": 6,
+    "name": "Zelenia Yang",
+    "username": "et, rutrum",
+    "email": "mi@ultricesVivamus.co.uk"
+  },
+  {
+    "id": 7,
+    "name": "Jeremy Gallegos",
+    "username": "purus. Duis",
+    "email": "sit@parturientmontes.net"
+  },
+  {
+    "id": 8,
+    "name": "Karyn Mcfarland",
+    "username": "sapien. Cras",
+    "email": "massa.Integer@neque.co.uk"
+  },
+  {
+    "id": 9,
+    "name": "Zenia Carlson",
+    "username": "enim. Suspendisse",
+    "email": "Quisque@sodaleselit.co.uk"
+  },
+  {
+    "id": 10,
+    "name": "Madeson Curtis",
+    "username": "orci luctus",
+    "email": "urna.nec.luctus@atvelit.edu"
+  },
+  {
+    "id": 11,
+    "name": "Ann Ramos",
+    "username": "ornare. In",
+    "email": "scelerisque.mollis@lobortisnisi.ca"
+  },
+  {
+    "id": 12,
+    "name": "Prescott Ellison",
+    "username": "orci, consectetuer",
+    "email": "mollis.non.cursus@necdiamDuis.org"
+  },
+  {
+    "id": 13,
+    "name": "Lucius Booth",
+    "username": "urna et",
+    "email": "tempus.scelerisque@ornareliberoat.edu"
+  },
+  {
+    "id": 14,
+    "name": "Skyler Gates",
+    "username": "nisl. Nulla",
+    "email": "arcu.Morbi.sit@Duisacarcu.com"
+  },
+  {
+    "id": 15,
+    "name": "Dane Sims",
+    "username": "neque pellentesque",
+    "email": "odio@Phasellusvitae.com"
+  },
+  {
+    "id": 16,
+    "name": "Rebecca Reed",
+    "username": "Vestibulum ante",
+    "email": "fames@laoreetlectus.ca"
+  },
+  {
+    "id": 17,
+    "name": "Reed Madden",
+    "username": "Donec fringilla.",
+    "email": "aliquet.sem.ut@ipsum.com"
+  },
+  {
+    "id": 18,
+    "name": "Fitzgerald Wilkerson",
+    "username": "dapibus ligula.",
+    "email": "in.molestie.tortor@Cumsociis.co.uk"
+  },
+  {
+    "id": 19,
+    "name": "Drew Garza",
+    "username": "Pellentesque habitant",
+    "email": "Aenean.euismod@ridiculusmusAenean.org"
+  },
+  {
+    "id": 20,
+    "name": "Holly Carter",
+    "username": "euismod est",
+    "email": "Nunc.mauris@anteNunc.edu"
+  },
+  {
+    "id": 21,
+    "name": "Malcolm Austin",
+    "username": "nunc sit",
+    "email": "leo.Cras@erat.edu"
+  },
+  {
+    "id": 22,
+    "name": "Fitzgerald Ward",
+    "username": "turpis vitae",
+    "email": "porttitor@fringillamilacinia.ca"
+  },
+  {
+    "id": 23,
+    "name": "Vaughan Pace",
+    "username": "amet risus.",
+    "email": "natoque.penatibus.et@erat.ca"
+  },
+  {
+    "id": 24,
+    "name": "Sharon Langley",
+    "username": "a neque.",
+    "email": "vitae.odio.sagittis@id.ca"
+  },
+  {
+    "id": 25,
+    "name": "Jason Chase",
+    "username": "eu elit.",
+    "email": "augue.Sed.molestie@ultricesposuerecubilia.com"
+  },
+  {
+    "id": 26,
+    "name": "Grant Kane",
+    "username": "Donec non",
+    "email": "adipiscing.lacus.Ut@sagittislobortismauris.edu"
+  },
+  {
+    "id": 27,
+    "name": "Aladdin Mccormick",
+    "username": "Curae; Donec",
+    "email": "mollis@nibhsit.net"
+  },
+  {
+    "id": 28,
+    "name": "Octavius Warner",
+    "username": "blandit. Nam",
+    "email": "risus.quis.diam@erategetipsum.net"
+  },
+  {
+    "id": 29,
+    "name": "Graiden Hughes",
+    "username": "laoreet lectus",
+    "email": "penatibus@Fuscealiquamenim.edu"
+  },
+  {
+    "id": 30,
+    "name": "Kyle Ortega",
+    "username": "orci lobortis",
+    "email": "arcu.Vivamus@Innec.ca"
+  },
+  {
+    "id": 31,
+    "name": "Mikayla Riggs",
+    "username": "eu, ligula.",
+    "email": "Aliquam.auctor.velit@ut.org"
+  },
+  {
+    "id": 32,
+    "name": "Belle Welch",
+    "username": "elit. Nulla",
+    "email": "ante@purusmaurisa.com"
+  },
+  {
+    "id": 33,
+    "name": "Odysseus Figueroa",
+    "username": "dictum ultricies",
+    "email": "Pellentesque@cursusaenim.co.uk"
+  },
+  {
+    "id": 34,
+    "name": "Jade Arnold",
+    "username": "mollis vitae,",
+    "email": "at.velit.Pellentesque@velitin.co.uk"
+  },
+  {
+    "id": 35,
+    "name": "Zachery Hess",
+    "username": "ornare lectus",
+    "email": "id.erat@Namnullamagna.com"
+  },
+  {
+    "id": 36,
+    "name": "Elliott Pena",
+    "username": "Nunc mauris",
+    "email": "mollis.vitae.posuere@adipiscing.edu"
+  },
+  {
+    "id": 37,
+    "name": "Kasimir Randall",
+    "username": "urna convallis",
+    "email": "Nulla@Proinnislsem.edu"
+  },
+  {
+    "id": 38,
+    "name": "Whitney Velazquez",
+    "username": "id enim.",
+    "email": "aliquet.vel@sapien.net"
+  },
+  {
+    "id": 39,
+    "name": "Garrison Knox",
+    "username": "Quisque libero",
+    "email": "nibh.lacinia@noncursusnon.org"
+  },
+  {
+    "id": 40,
+    "name": "Jeremy Bowen",
+    "username": "metus. Vivamus",
+    "email": "Fusce.feugiat@a.org"
+  },
+  {
+    "id": 41,
+    "name": "Hillary Bailey",
+    "username": "est, vitae",
+    "email": "Ut.sagittis.lobortis@odioEtiamligula.edu"
+  },
+  {
+    "id": 42,
+    "name": "Larissa Pruitt",
+    "username": "ut erat.",
+    "email": "dictum@auguemalesuadamalesuada.ca"
+  },
+  {
+    "id": 43,
+    "name": "Rae Gregory",
+    "username": "ante bibendum",
+    "email": "diam.Duis.mi@massarutrum.net"
+  },
+  {
+    "id": 44,
+    "name": "Alma Mcneil",
+    "username": "nisl sem,",
+    "email": "elementum.sem@CurabiturmassaVestibulum.com"
+  },
+  {
+    "id": 45,
+    "name": "Jonas Richards",
+    "username": "et libero.",
+    "email": "nisi.Cum.sociis@purus.com"
+  },
+  {
+    "id": 46,
+    "name": "Kieran Garrison",
+    "username": "bibendum. Donec",
+    "email": "consectetuer.rhoncus@vitaesodales.co.uk"
+  },
+  {
+    "id": 47,
+    "name": "Barrett Cole",
+    "username": "Pellentesque tincidunt",
+    "email": "commodo.hendrerit.Donec@magna.com"
+  },
+  {
+    "id": 48,
+    "name": "Ira Keith",
+    "username": "amet ornare",
+    "email": "dui.lectus@duilectusrutrum.com"
+  },
+  {
+    "id": 49,
+    "name": "Barclay Kelly",
+    "username": "sem elit,",
+    "email": "vestibulum.neque.sed@libero.net"
+  },
+  {
+    "id": 50,
+    "name": "Justine Patton",
+    "username": "quam. Curabitur",
+    "email": "consequat.enim@vestibulum.net"
+  },
+  {
+    "id": 51,
+    "name": "Nasim Andrews",
+    "username": "mi. Duis",
+    "email": "nisi.Cum.sociis@idnuncinterdum.org"
+  },
+  {
+    "id": 52,
+    "name": "Brynn Hatfield",
+    "username": "adipiscing fringilla,",
+    "email": "per.conubia.nostra@convallisconvallis.com"
+  },
+  {
+    "id": 53,
+    "name": "Sage Snow",
+    "username": "sem ut",
+    "email": "elementum.dui@sitamet.ca"
+  },
+  {
+    "id": 54,
+    "name": "Wynter Burgess",
+    "username": "non, dapibus",
+    "email": "netus.et.malesuada@tortorNunc.ca"
+  },
+  {
+    "id": 55,
+    "name": "Carissa Frost",
+    "username": "faucibus orci",
+    "email": "malesuada@vitaediam.ca"
+  },
+  {
+    "id": 56,
+    "name": "Ramona Fox",
+    "username": "a neque.",
+    "email": "convallis.dolor@semperet.net"
+  },
+  {
+    "id": 57,
+    "name": "Hadassah Wood",
+    "username": "sagittis. Nullam",
+    "email": "Suspendisse.aliquet.molestie@lacuspede.ca"
+  },
+  {
+    "id": 58,
+    "name": "Lani Albert",
+    "username": "amet, consectetuer",
+    "email": "ornare.elit@malesuadamalesuadaInteger.ca"
+  },
+  {
+    "id": 59,
+    "name": "Dylan Gray",
+    "username": "Quisque varius.",
+    "email": "dui.Fusce@pedeultricesa.edu"
+  },
+  {
+    "id": 60,
+    "name": "Kenyon Lamb",
+    "username": "urna. Nullam",
+    "email": "ullamcorper@nulla.co.uk"
+  },
+  {
+    "id": 61,
+    "name": "Katelyn Duran",
+    "username": "quis accumsan",
+    "email": "Mauris@euismod.edu"
+  },
+  {
+    "id": 62,
+    "name": "Kylynn Whitley",
+    "username": "Morbi vehicula.",
+    "email": "Nulla.tincidunt.neque@non.org"
+  },
+  {
+    "id": 63,
+    "name": "Richard Campbell",
+    "username": "cursus. Nunc",
+    "email": "Phasellus@mauris.co.uk"
+  },
+  {
+    "id": 64,
+    "name": "Quon Gilmore",
+    "username": "aliquet diam.",
+    "email": "Duis@arcu.co.uk"
+  },
+  {
+    "id": 65,
+    "name": "Nero Sparks",
+    "username": "Pellentesque habitant",
+    "email": "a@ligulaAliquamerat.ca"
+  },
+  {
+    "id": 66,
+    "name": "Amela Arnold",
+    "username": "mi enim,",
+    "email": "Fusce@maurisut.com"
+  },
+  {
+    "id": 67,
+    "name": "Megan Holcomb",
+    "username": "viverra. Donec",
+    "email": "ante@acnulla.org"
+  },
+  {
+    "id": 68,
+    "name": "Xanthus Sutton",
+    "username": "eu lacus.",
+    "email": "dis@consectetuermaurisid.org"
+  },
+  {
+    "id": 69,
+    "name": "Maile Dyer",
+    "username": "et netus",
+    "email": "Aenean.eget@fermentummetusAenean.org"
+  },
+  {
+    "id": 70,
+    "name": "Nero Galloway",
+    "username": "ut, sem.",
+    "email": "sapien.gravida.non@hendreritconsectetuercursus.co.uk"
+  },
+  {
+    "id": 71,
+    "name": "Keane Frederick",
+    "username": "ullamcorper. Duis",
+    "email": "netus.et.malesuada@eratneque.co.uk"
+  },
+  {
+    "id": 72,
+    "name": "Dora Sargent",
+    "username": "pede. Nunc",
+    "email": "Mauris.nulla.Integer@tellusloremeu.co.uk"
+  },
+  {
+    "id": 73,
+    "name": "Anika Mcleod",
+    "username": "dui nec",
+    "email": "eget.mollis@at.net"
+  },
+  {
+    "id": 74,
+    "name": "Amber House",
+    "username": "Aliquam nisl.",
+    "email": "gravida.sit@at.edu"
+  },
+  {
+    "id": 75,
+    "name": "Chadwick Paul",
+    "username": "Nullam feugiat",
+    "email": "dolor@elit.edu"
+  },
+  {
+    "id": 76,
+    "name": "Shafira Sherman",
+    "username": "odio tristique",
+    "email": "tempor.erat@commodotinciduntnibh.com"
+  },
+  {
+    "id": 77,
+    "name": "Whoopi Jackson",
+    "username": "Morbi neque",
+    "email": "in@dolor.co.uk"
+  },
+  {
+    "id": 78,
+    "name": "Brock Browning",
+    "username": "nibh. Aliquam",
+    "email": "at@enim.org"
+  },
+  {
+    "id": 79,
+    "name": "Karen Dixon",
+    "username": "id, mollis",
+    "email": "luctus@nibh.co.uk"
+  },
+  {
+    "id": 80,
+    "name": "Hope Finley",
+    "username": "semper, dui",
+    "email": "sagittis@magna.net"
+  },
+  {
+    "id": 81,
+    "name": "Camden Summers",
+    "username": "posuere, enim",
+    "email": "pede.et@nisidictum.org"
+  },
+  {
+    "id": 82,
+    "name": "Jack Reeves",
+    "username": "Pellentesque habitant",
+    "email": "sociosqu.ad.litora@et.com"
+  },
+  {
+    "id": 83,
+    "name": "Chastity Holland",
+    "username": "risus odio,",
+    "email": "arcu.imperdiet@nullaante.net"
+  },
+  {
+    "id": 84,
+    "name": "Fleur Kline",
+    "username": "malesuada fringilla",
+    "email": "fringilla.cursus.purus@venenatisa.edu"
+  },
+  {
+    "id": 85,
+    "name": "Gray Houston",
+    "username": "adipiscing elit.",
+    "email": "arcu.Vivamus.sit@utquam.ca"
+  },
+  {
+    "id": 86,
+    "name": "Fritz Melendez",
+    "username": "Cras sed",
+    "email": "hendrerit@magnisdisparturient.ca"
+  },
+  {
+    "id": 87,
+    "name": "Alan Ayers",
+    "username": "sit amet,",
+    "email": "dictum.mi.ac@anuncIn.edu"
+  },
+  {
+    "id": 88,
+    "name": "Lysandra Spence",
+    "username": "massa lobortis",
+    "email": "at.sem.molestie@necanteMaecenas.edu"
+  },
+  {
+    "id": 89,
+    "name": "Len Wright",
+    "username": "porttitor eros",
+    "email": "iaculis@ProinmiAliquam.org"
+  },
+  {
+    "id": 90,
+    "name": "Tanya Mcmahon",
+    "username": "et netus",
+    "email": "eu.accumsan.sed@malesuadafamesac.co.uk"
+  },
+  {
+    "id": 91,
+    "name": "Dorian Perez",
+    "username": "aptent taciti",
+    "email": "at@tellusimperdiet.net"
+  },
+  {
+    "id": 92,
+    "name": "Sarah Combs",
+    "username": "Vivamus sit",
+    "email": "magna@euismod.co.uk"
+  },
+  {
+    "id": 93,
+    "name": "Byron Becker",
+    "username": "egestas. Aliquam",
+    "email": "tincidunt.nibh@nibhdolor.edu"
+  },
+  {
+    "id": 94,
+    "name": "Karleigh Joyce",
+    "username": "pellentesque, tellus",
+    "email": "vitae.nibh.Donec@liberoProin.edu"
+  },
+  {
+    "id": 95,
+    "name": "Imani Wiggins",
+    "username": "nec, diam.",
+    "email": "Sed.dictum.Proin@etipsum.co.uk"
+  },
+  {
+    "id": 96,
+    "name": "Genevieve Chang",
+    "username": "Curabitur egestas",
+    "email": "nonummy.ultricies.ornare@arcu.ca"
+  },
+  {
+    "id": 97,
+    "name": "Sacha Warner",
+    "username": "et pede.",
+    "email": "lobortis@malesuadaaugueut.edu"
+  },
+  {
+    "id": 98,
+    "name": "Rafael Stokes",
+    "username": "Quisque nonummy",
+    "email": "pede.blandit@disparturient.ca"
+  },
+  {
+    "id": 99,
+    "name": "Elvis Wynn",
+    "username": "eu, accumsan",
+    "email": "molestie.Sed@nonummyipsumnon.edu"
+  },
+  {
+    "id": 100,
+    "name": "Shelby Mcintosh",
+    "username": "nisi magna",
+    "email": "Ut.tincidunt@liberonecligula.ca"
+  }
+];
